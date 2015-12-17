4c26c95f46389ced3e1725043e9311adf3e57040
Dave Ho <imdaveho@gmail.com>
2015-12-17 12:47:54 +0000

    changed django-bp v0.1 app settings with default sqlite3 db

--- django-boilerplate/project_name/settings/application.py
+++ django-boilerplate/project_name/settings/application.py
@@ -27,9 +27,16 @@ MANAGERS = ADMINS + [
 ]
 
 # Application Database Settings:
-import dj_database_url
+# import dj_database_url
+# DATABASES = {
+#     "default": dj_database_url.config(default="postgresql://"),
+# }
+# # Default Django DB Config (SQLite3)
 DATABASES = {
-    "default": dj_database_url.config(default="postgresql://"),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+	"NAME": os.path.join(BASE_DIR, "db.sqlite3")
+    }
 }
 
 # See https://github.com/etianen/django-herokuapp/blob/master/
